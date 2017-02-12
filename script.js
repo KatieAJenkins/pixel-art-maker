c42b860e0274a32e2d07c064b0260948e672c43c
KatieAJenkins <kind2karma@gmail.com>
2017-02-12 16:54:16 -0700

    purple is working

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 'use strict';
 
-
 var squares = document.querySelector('#myGrid');
 var red = document.querySelector('#red');
-console.log(red);
+// console.log(red);
 var blue = document.querySelector('#blue');
-console.log(blue);
+// console.log(blue);
+var purple = document.querySelector('#purple');
+// console.log(blue);
 
 var brushColor = "red";
 
 //set square background to brushColor
 var changeColor = function () {
   if (event.target !== event.currentTarget){
-    console.log(event.target);
+    // console.log(event.target);
     event.target.setAttribute("style" , "background-color:" + brushColor);
-    console.log(brushColor);
+    // console.log(brushColor);
   }
 };
 
@@ -23,25 +24,16 @@ squares.addEventListener('click', changeColor, false);
 
 //event listeners on colorPalette
 red.addEventListener('click', function() {
+  brushColor = "red";
   console.log('red');
-  // console.log(event.target);
-  // .setAttribute("style" , "background-color:red")
-  // console.log('red');
-  // var brushColor = 'red';
 });
 
 blue.addEventListener('click' , function(){
   brushColor = "blue";
-  // .getElementById('blue')
-      // event.target.setAttribute("style" , "background-color:" + brushColor);
-  // console.log(brushColor);
   console.log('blue');
 });
 
-
-// var red = document.querySelector("#red");
-// console.log(red);
-//
-// red.addEventListener('click', function() {
-//   console.log("red");
-//   event.target.setAttribute('style' , "background-color:red");
+purple.addEventListener('click' , function(){
+  brushColor = "purple";
+  console.log('purple');
+});
